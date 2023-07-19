ebf26660516b485bb268f2b8e5736a35cb5a62ff
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-07-19 19:50:48 -0300

    refactor: removed api-key from file

--- src/utils/geocoding.js
+++ src/utils/geocoding.js
@@ -1,13 +1,28 @@
-export default async function geoCoding(city='paris') {
-  const URL = `https://api.api-ninjas.com/v1/city?name=${city}`;
-  const response = await fetch(URL, {
-    headers: {
-      'X-Api-Key': 'eRm7V348yj92eKDtgO5DYg==lnR8PQEqdohPDqQM'
+function coordinatesFetcher() {
+  async function geoCoding(city='belo horizonte') {
+    const URL = `https://api.api-ninjas.com/v1/city?name=${city}`;
+    const response = await fetch(URL, {
+      headers: {
+        'X-Api-Key': process.env.NINJAS_APIKEY
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch');
     }
-  });
-  if (!response.ok) {
-    throw new Error('Failed to fetch');
+
+    const data = await response.json();
+    return data;
   }
-  const data = await response.json();
-  return data;
+  
+  async function getCurrentISSLocation() {
+    const ISS_BASE_API = 'http://api.open-notify.org';
+    const response = await fetch(`${ISS_BASE_API}/iss-now.json`);
+    const { iss_position: issPosition } = await response.json();
+    return issPosition;
+  }
+
+  return { geoCoding, getCurrentISSLocation }
 }
+
+export default coordinatesFetcher()
