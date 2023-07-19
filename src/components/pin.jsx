105f74ffbb6af080647c8a4a45d0262688e98b81
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-07-19 20:02:16 -0300

    refactor: pin radius change
    feat: city coordinates dictated by the earth component

--- src/components/pin.jsx
+++ src/components/pin.jsx
@@ -1,13 +1,19 @@
 import calculate from '@/utils/coordinatesConverter';
-import geoCoding from '@/utils/geocoding';
+import coordinatesFetcher from '@/utils/geocoding';
 import React, { useEffect, useState } from 'react'
 
-function Pin() {
+function Pin({ city }) {
   const [{ x, y, z }, setCoordinates] = useState({ x: 0, y: 0, z: 0 });
   
   useEffect(() => {
     (async () => {
-      const [{ latitude, longitude }] = await geoCoding('Caruaru');
+      if (!city || city.length < 0) {
+        const { x, y, z } = calculate().coordinates();
+        setCoordinates({ x, y, z });
+        return;
+      }
+
+      const [{ latitude, longitude }] = await coordinatesFetcher.geoCoding(city);
       const { x, y, z } = calculate(latitude, longitude).coordinates();
       setCoordinates({ x, y, z });
     })();
@@ -17,7 +23,7 @@ function Pin() {
     <mesh
       position={[x, y, z]}
     >
-      <sphereGeometry args={[0.005, 2, 2]} />
+      <sphereGeometry args={[0.008, 3, 2]} />
       <meshBasicMaterial
         color={ 0xfff0000 }
       />
