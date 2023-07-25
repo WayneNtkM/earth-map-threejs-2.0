1c64d1c7f0bf23a6220c69050cd5e1c7fdf726c1
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-07-25 12:26:13 -0300

    feat: iss and earth is now part of the user options

--- src/components/earth.jsx
+++ src/components/earth.jsx
@@ -1,18 +1,23 @@
-import React, { useRef, lazy } from "react";
+import React, { useRef, lazy, useContext } from "react";
 import { useFrame } from "@react-three/fiber";
 import { OrbitControls, Stars, useTexture } from "@react-three/drei";
 import * as THREE from "three";
-const Pin = lazy(() => import('@/components/pin'));
+// const Pin = lazy(() => import('@/components/pin'));
 const Iss = lazy(() => import('@/components/iss'));
+import { MenuContext } from './user-options/menu';
 
 export function Earth() {
   const [colorMap, normalMap, specularMap, cloudsMap, nightMap] = useTexture([
-    '/8k_earth_daymap.jpeg',
+    '8k_earth_daymap.jpeg',
     '8k_earth_normal_map.jpeg',
     '8k_earth_specular_map.jpeg',
     '8k_earth_clouds.jpg',
     '8k_earth_nightmap.jpeg'
   ]);
+  const { options } = useContext(MenuContext);
+  const { rotate: { earth, clouds } } = options;
+  const { isRotating, speed } = earth;
+  console.log(earth);
 
   [colorMap, normalMap, specularMap, cloudsMap].map((e) => {
     e.wrapS = THREE.RepeatWrapping;
@@ -26,8 +31,8 @@ export function Earth() {
   useFrame(({ clock }) => {
     const elapsedTime = clock.getElapsedTime();
 
-    // earthRef.current.rotation.y = elapsedTime / 24;
-    cloudsRef.current.rotation.y = elapsedTime / 24;
+    earthRef.current.rotation.y = isRotating && elapsedTime / ((24 / speed.accelerate) * 60 * 60);
+    cloudsRef.current.rotation.y = clouds.isRotating && elapsedTime / ((24 / clouds.speed.accelerate) * 60 * 60);
   });
 
   return (
@@ -41,16 +46,27 @@ export function Earth() {
         args={[0xFFFFFF, 1]}
         position={[-100, 10, 50]}
       /> */}
+      <OrbitControls
+          enableZoom={true}
+          enableRotate={true}
+          zoomSpeed={0.2}
+          panSpeed={0.5}
+          rotateSpeed={0.4}
+          target={[0, 0, 3]}
+          target0={[0, 0, 3]}
+          minDistance={1.3}
+          maxDistance={40}
+        />
       <Stars
         radius={300}
         depth={60}
-        count={20000}
+        count={1000}
         factor={7}
         saturation={0}
         fade={true}
       />
       <mesh ref={cloudsRef} position={[0, 0, 3]}>
-        <sphereGeometry args={[1.005, 32, 32]} />
+        <sphereGeometry args={[0.91, 32, 32]} />
         <meshPhongMaterial
           map={cloudsMap}
           opacity={0.4}
@@ -60,28 +76,17 @@ export function Earth() {
         />
       </mesh>
       <mesh ref={earthRef} position={[0, 0, 3]}>
-        <sphereGeometry args={[1, 32, 32]} />
+        <sphereGeometry args={[0.9, 32, 32]} />
         <meshPhongMaterial specularMap={specularMap} />
         <meshStandardMaterial
           map={colorMap}
           normalMap={normalMap}
-          metalness={0.4}
-          roughness={0.7}
+          metalness={0.1}
+          roughness={0.8}
       />
-        <OrbitControls
-          enableZoom={true}
-          enablePan={true}
-          enableRotate={true}
-          zoomSpeed={0.2}
-          panSpeed={0.5}
-          rotateSpeed={0.4}
-          target={[0, 0, 3]}
-          minDistance={1.3}
-          maxDistance={40}
-        />
-        {['santiago', 'sydney', 'rio de janeiro'].map((city, index) => (
+        {/* {['santiago', 'sydney', 'rio de janeiro'].map((city, index) => (
           <Pin city={city} key={index} />
-          ))}
+          ))} */}
         <Iss />
       </mesh>
     </>
